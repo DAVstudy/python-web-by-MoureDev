6dbf347c8f7a9bfe4ab7a995e00c9eaf5f49c059
DIEGO ARENAS <arenasvega.diego@gmail.com>
2024-05-21 19:23:53 -0400

    CORS

--- link_bio/rxconfig.py
+++ link_bio/rxconfig.py
@@ -2,4 +2,6 @@ import reflex as rx
 
 config = rx.Config(
     app_name="link_bio",
+    api_url="https://devsdav-web.up.railway.app/",
+    cors_allowed_origins=["https://devsdav-web.vercel.app/", "http://localhost:3000/"]
 )
